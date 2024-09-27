49dc23ebc668ae4a7eeeeb55a3d31c68cf5e5020
rizky21-max <riskiivansyah51@gmail.com>
2024-09-27 10:58:21 +0700

    Add files via upload

---
+++ tipe data string rizkyr.py
@@ -0,0 +1,13 @@
+nama_depan = "Rizky"
+nama_belakang = 'Rivansyah'
+nama_lengkap = nama_depan + '' + nama_belakang
+usia = 19
+alamat = 'Parittiga'
+kata_mutiara = "Jadilah diri sendiri"
+
+print(nama_lengkap, '(' + usia + ')', ', ', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print(',Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
